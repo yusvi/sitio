7324067bc1dc7a710e56fb09a3e37c3acf0cd9c1
yusvi <isc.yusvizareth.leon.cruz@gmail.com>
2017-01-19 02:29:54 -0800

    Update views.py

--- smart_selects/views.py
+++ smart_selects/views.py
@@ -1,14 +1,13 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.db.models import Q
+from django.utils.six import iteritems
+from django.views.decorators.cache import never_cache
+
 try:
     from django.apps import apps
     get_model = apps.get_model
 except ImportError:
     from django.db.models.loading import get_model
-
-try:
-    import json
-except ImportError:
-    from django.utils import simplejson as json
 
 from smart_selects.utils import (get_keywords, sort_results, serialize_results,
                                  get_queryset, get_limit_choices_to)
@@ -33,6 +32,27 @@ def is_m2m(model_class, field):
     return False
 
 
+def do_filter(qs, keywords, exclude=False):
+    """
+    Filter queryset based on keywords.
+    Support for multiple-selected parent values.
+    """
+    and_q = Q()
+    for keyword, value in iteritems(keywords):
+        values = value.split(",")
+        if len(values) > 0:
+            or_q = Q()
+            for value in values:
+                or_q |= Q(**{keyword: value})
+            and_q &= or_q
+    if exclude:
+        qs = qs.exclude(and_q)
+    else:
+        qs = qs.filter(and_q)
+    return qs
+
+
+@never_cache
 def filterchain(request, app, model, field, foreign_key_app_name, foreign_key_model_name,
                 foreign_key_field_name, value, manager=None):
     model_class = get_model(app, model)
@@ -42,7 +62,7 @@ def filterchain(request, app, model, field, foreign_key_app_name, foreign_key_mo
     limit_choices_to = get_limit_choices_to(foreign_key_app_name, foreign_key_model_name, foreign_key_field_name)
     queryset = get_queryset(model_class, manager, limit_choices_to)
 
-    results = queryset.filter(**keywords)
+    results = do_filter(queryset, keywords)
 
     # Sort results if model doesn't include a default ordering.
     if not getattr(model_class._meta, 'ordering', False):
@@ -50,10 +70,10 @@ def filterchain(request, app, model, field, foreign_key_app_name, foreign_key_mo
         sort_results(results)
 
     serialized_results = serialize_results(results)
-    results_json = json.dumps(serialized_results)
-    return HttpResponse(results_json, content_type='application/json')
+    return JsonResponse(serialized_results, safe=False)
 
 
+@never_cache
 def filterchain_all(request, app, model, field, foreign_key_app_name,
                     foreign_key_model_name, foreign_key_field_name, value):
     """Returns filtered results followed by excluded results below."""
@@ -63,11 +83,15 @@ def filterchain_all(request, app, model, field, foreign_key_app_name,
     limit_choices_to = get_limit_choices_to(foreign_key_app_name, foreign_key_model_name, foreign_key_field_name)
     queryset = get_queryset(model_class, limit_choices_to=limit_choices_to)
 
-    filtered = list(queryset.filter(**keywords))
-    sort_results(filtered)
+    filtered = list(do_filter(queryset, keywords))
+    # Sort results if model doesn't include a default ordering.
+    if not getattr(model_class._meta, 'ordering', False):
+        sort_results(list(filtered))
 
-    excluded = list(queryset.exclude(**keywords))
-    sort_results(excluded)
+    excluded = list(do_filter(queryset, keywords, exclude=True))
+    # Sort results if model doesn't include a default ordering.
+    if not getattr(model_class._meta, 'ordering', False):
+        sort_results(list(excluded))
 
     # Empty choice to separate filtered and excluded results.
     empty_choice = {'value': "", 'display': "---------"}
@@ -78,5 +102,5 @@ def filterchain_all(request, app, model, field, foreign_key_app_name,
         serialize_results(excluded)
     )
 
-    results_json = json.dumps(serialized_results)
-    return HttpResponse(results_json, content_type='application/json')
+    return JsonResponse(serialized_results, safe=False)
+ 
