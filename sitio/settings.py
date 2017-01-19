7a7248441f98da635803d411684c3d617e86d03d
yusvi <isc.yusvizareth.leon.cruz@gmail.com>
2017-01-18 20:02:10 -0800

    Update settings.py

--- sitio/settings.py
+++ sitio/settings.py
@@ -92,16 +92,26 @@ WSGI_APPLICATION = 'sitio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.mysql',
+   #     'NAME': 'db_home',
+    #    'USER': 'root',
+     #   'PASSWORD': 'grafiti12',
+      #  'HOST': '',
+       # 'PORT': '',
+    #}
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_home',
-        'USER': 'root',
-        'PASSWORD': 'grafiti12',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 
 
 # Password validation
