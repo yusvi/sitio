7759cd6df769355004b9d19e7b6501bd89f9ab01
yusvi <isc.yusvizareth.leon.cruz@gmail.com>
2017-01-18 20:10:48 -0800

    Update urls.py

--- sitio/urls.py
+++ sitio/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import include, url,patterns
+#from django.conf.urls import include, url,patterns
+from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 
