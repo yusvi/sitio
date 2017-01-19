22c8729dd1df9f6cee3752ab711c0fba76d467b4
yusvi <isc.yusvizareth.leon.cruz@gmail.com>
2017-01-19 00:33:03 -0800

    Update settings.py

--- sitio/settings.py
+++ sitio/settings.py
@@ -72,8 +72,7 @@ ROOT_URLCONF = 'sitio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"static","templates"),
-                 ],
+        'DIRS': ['/home/yusvi/sitio/home/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
